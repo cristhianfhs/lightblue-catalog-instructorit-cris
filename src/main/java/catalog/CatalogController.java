ef161db3afadf7889f2ef1606893a7fe2f5e6ce9
Cristhian Fernando Hernandez Salgado <chernandez@instructorit.com>
2019-08-08 02:04:56 +0000

    Connect to MySQL.

--- src/main/java/catalog/CatalogController.java
+++ src/main/java/catalog/CatalogController.java
@@ -10,29 +10,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//Added
+import catalog.models.*;
+import org.springframework.beans.factory.annotation.Autowired;
+//Finish the Added
+
 /**
  * REST Controller to manage Inventory database
  */
 @RestController
 public class CatalogController {
+	
+	//Added
+	@Autowired
+    InventoryRepo itemsRepo;
+    //Finish the Added
 
     Logger logger = LoggerFactory.getLogger(CatalogController.class);
 
     /**
      * @return all items in inventory
      */
-    @RequestMapping(value = "/items", method = RequestMethod.GET)
+    //BEFORE EXERCISE
+    /*@RequestMapping(value = "/items", method = RequestMethod.GET)
     @ResponseBody
     ResponseEntity<?> getInventory() {
         return ResponseEntity.ok("[{\"id\": 1,\"name\":\"one\"},{\"id\":2,\"name\":\"two\"}]");
-    }
+    }*/
+   //ADDED
+   @RequestMapping(value = "/items", method = RequestMethod.GET)
+   @ResponseBody
+   Iterable<Inventory> getInventory() {
+     return itemsRepo.findAll();
+   }
 
     /**
      * @return item by id
      */
-    @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
+    //BEFORE VERSION
+    /*@RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
     ResponseEntity<?> getById(@PathVariable long id) {
                 return ResponseEntity.ok("{\"id\":1,\"name\":\"one\"}");
-    }
+    }*/
+   //ADDED
+   @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
+  ResponseEntity<?> getById(@PathVariable long id) {
+    if (!itemsRepo.exists(id)) {
+       return ResponseEntity.notFound().build();
+   	}
+    return ResponseEntity.ok(itemsRepo.findOne(id));
+  }
 
 }
