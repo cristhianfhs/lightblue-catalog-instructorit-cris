ff52a8844474e5783717330c604b1440374b5738
Cristhian Fernando Hernandez Salgado <chernandez@instructorit.com>
2019-08-08 03:37:31 +0000

    Fixing the NotNull package.

--- src/main/java/catalog/Inventory.java
+++ src/main/java/catalog/Inventory.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constrains.NotNull;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="items")
