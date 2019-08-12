4e1b61f424f710e2d349206d312e2af1888f1de7
Cristhian Fernando Hernandez Salgado <chernandez@instructorit.com>
2019-08-12 16:35:13 +0000

    Chamging the principal code

--- src/main/java/catalog/CatalogController.java
+++ src/main/java/catalog/CatalogController.java
@@ -1,8 +1,10 @@
 package catalog;
 
+
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,56 +12,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-//Added
-//import catalog.models.*;
-import catalog.*;
-import org.springframework.beans.factory.annotation.Autowired;
-//Finish the Added
-
 /**
  * REST Controller to manage Inventory database
  */
 @RestController
 public class CatalogController {
-	
-	//Added
-	@Autowired
-    InventoryRepo itemsRepo;
-    //Finish the Added
 
     Logger logger = LoggerFactory.getLogger(CatalogController.class);
+
+    @Autowired
+    InventoryRepo itemsRepo;
 
     /**
      * @return all items in inventory
      */
-    //BEFORE EXERCISE
-    /*@RequestMapping(value = "/items", method = RequestMethod.GET)
+    @RequestMapping(value = "/items", method = RequestMethod.GET)
     @ResponseBody
-    ResponseEntity<?> getInventory() {
-        return ResponseEntity.ok("[{\"id\": 1,\"name\":\"one\"},{\"id\":2,\"name\":\"two\"}]");
-    }*/
-   //ADDED
-   @RequestMapping(value = "/items", method = RequestMethod.GET)
-   @ResponseBody
-   Iterable<Inventory> getInventory() {
-     return itemsRepo.findAll();
-   }
+    Iterable<Inventory> getInventory() {
+        return itemsRepo.findAll();
+    }
 
     /**
      * @return item by id
      */
-    //BEFORE VERSION
-    /*@RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
     ResponseEntity<?> getById(@PathVariable long id) {
-                return ResponseEntity.ok("{\"id\":1,\"name\":\"one\"}");
-    }*/
-   //ADDED
-   @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
-  ResponseEntity<?> getById(@PathVariable long id) {
-    if (!itemsRepo.exists(id)) {
-       return ResponseEntity.notFound().build();
-   	}
-    return ResponseEntity.ok(itemsRepo.findOne(id));
-  }
+                if (!itemsRepo.exists(id)) {
+                        return ResponseEntity.notFound().build();
+                }
+
+                return ResponseEntity.ok(itemsRepo.findOne(id));
+    }
 
 }
