3188b5fce7857a9332c0588365a2c675a7c371db
Cristhian Fernando Hernandez Salgado <chernandez@instructorit.com>
2019-08-12 16:45:54 +0000

    We are trying to change the infoemation about the inventoty.

--- src/main/java/catalog/CatalogController.java
+++ src/main/java/catalog/CatalogController.java
@@ -1,10 +1,8 @@
 package catalog;
 
-
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,36 +10,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//Added
+//import catalog.models.*;
+import catalog.*;
+import org.springframework.beans.factory.annotation.Autowired;
+//Finish the Added
+
 /**
  * REST Controller to manage Inventory database
  */
 @RestController
 public class CatalogController {
+	
+	//Added
+	@Autowired
+    InventoryRepo itemsRepo;
+    //Finish the Added
 
     Logger logger = LoggerFactory.getLogger(CatalogController.class);
-
-    @Autowired
-    InventoryRepo itemsRepo;
 
     /**
      * @return all items in inventory
      */
-    @RequestMapping(value = "/items", method = RequestMethod.GET)
+    //BEFORE EXERCISE
+    /*@RequestMapping(value = "/items", method = RequestMethod.GET)
     @ResponseBody
-    Iterable<Inventory> getInventory() {
-        return itemsRepo.findAll();
-    }
+    ResponseEntity<?> getInventory() {
+        return ResponseEntity.ok("[{\"id\": 1,\"name\":\"one\"},{\"id\":2,\"name\":\"two\"}]");
+    }*/
+   //ADDED
+   @RequestMapping(value = "/items", method = RequestMethod.GET)
+   @ResponseBody
+   Iterable<Inventory> getInventory() {
+     return itemsRepo.findAll();
+   }
 
     /**
      * @return item by id
      */
-    @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
+    //BEFORE VERSION
+    /*@RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
     ResponseEntity<?> getById(@PathVariable long id) {
-                if (!itemsRepo.exists(id)) {
-                        return ResponseEntity.notFound().build();
-                }
-
-                return ResponseEntity.ok(itemsRepo.findOne(id));
-    }
+                return ResponseEntity.ok("{\"id\":1,\"name\":\"one\"}");
+    }*/
+   //ADDED
+   @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
+  ResponseEntity<?> getById(@PathVariable long id) {
+    if (!itemsRepo.exists(id)) {
+       return ResponseEntity.notFound().build();
+   	}
+    return ResponseEntity.ok(itemsRepo.findOne(id));
+  }
 
 }
--- src/main/java/catalog/Inventory.java
+++ src/main/java/catalog/Inventory.java
@@ -1,54 +1,55 @@
-
-//Now we need to create inventory.java.
-package catalog;
+package catalog.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.persistence.GenerationType;
 
+/**
+ * Entity: inventorydb.items
+ *
+ */
 @Entity
-@Table(name="items")
+@Table(name = "items")
 public class Inventory {
+  
   // Use generated ID
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
-
+  
   // Item name
   @NotNull
   private String name;
 
   // Item description
-  @NotNull 
+  @NotNull
   private String description;
 
-  //Item price
+  // Item price
   @NotNull
   private int price;
 
-  //Item img_alt
-  private String img_alt;
+  // Item img_alt
+  private String img_alt;  
 
-  //Item img
+  // Item img
   @NotNull
   private String img;
 
-  //Item stock
+  // Item stock
   @NotNull
   private int stock;
 
-  //The constructors
-
   public Inventory() { }
-  public Inventory(long id) {
+
+  public Inventory(long id) { 
     this.id = id;
-    }
+  }
   
-  public Inventory(String name, String description,
-  int price, String img_alt, String img, int stock) {
+  public Inventory(String name, String description, int price, String img_alt, String img, int stock) {
     this.name = name;
     this.description = description;
     this.price = price;
@@ -57,18 +58,74 @@ public class Inventory {
     this.stock = stock;
   }
 
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long value) {
+    this.id = value;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+  
+  public void setDescription(String value) {
+    this.description = value;
+  }
+  
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String value) {
+    this.name = value;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int value) {
+    this.price = value;
+  }
+  
+  public String getImg() {
+	  return img;
+  }
+  
+  public void setImg(String img) {
+	  this.img = img;
+  }
+
+  public String getImgAlt() {
+    return img_alt;
+  }
+  
+  public void setImgAlt(String img_alt) {
+    this.img_alt = img_alt;
+  }
+
+  public int getStock() {
+    return stock;
+  }
+
+  public void setStock(int value) {
+    this.stock = value;
+  }
+
   public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("{\n");
-    sb.append(String.format("\t\"id\": %s,\n",this.id));
-    sb.append(String.format("\t\"name\": \"%s\",\n",this.name));
-    sb.append(String.format("\t\"description\": \"%s\",\n",this.description));
-    sb.append(String.format("\t\"price\": %s,\n",this.price));
-    sb.append(String.format("\t\"imgAlt\": \"%s\",\n",this.img_alt));
-    sb.append(String.format("\t\"img\": \"%s\",\n",this.img));
-    sb.append(String.format("\t\"stock\": %s,\n",this.stock));
-    sb.append("}");
-    return sb.toString();
-   }
- }
- 
+    StringBuilder string = new StringBuilder();
+    string.append("{\n");
+    string.append(String.format("\t\"id\": %s,\n", this.id));
+    string.append(String.format("\t\"name\": \"%s\",\n", this.name));
+    string.append(String.format("\t\"description\": \"%s\",\n", this.description));
+    string.append(String.format("\t\"price\": %s,\n", this.price));
+    string.append(String.format("\t\"imgAlt\": \"%s\",\n", this.img_alt));
+    string.append(String.format("\t\"img\": \"%s\",\n", this.img));
+    string.append(String.format("\t\"stock\": %s\n", this.stock));
+    string.append("}");
+
+    return string.toString();
+  }
+}
