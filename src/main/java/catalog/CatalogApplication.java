920cfa741b249bdfc367231225459185c4fc7cd2
Cristhian Fernando Hernandez Salgado <chernandez@instructorit.com>
2019-09-30 19:19:41 +0000

    Hoollaalla

--- src/main/java/catalog/CatalogApplication.java
+++ src/main/java/catalog/CatalogApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatalogApplication {
-
+//Hola Mundo
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
 	}
